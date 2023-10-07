a5f0a0cb9741c97de34cafdb94936cb5e4dcca09
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-10-07 13:28:11 +0530

    setting up the form

--- src/app/components/inquiry.tsx
+++ src/app/components/inquiry.tsx
@@ -1,21 +1,55 @@
+"use client";
 import React from 'react'
+import { useState,useEffect } from 'react';
 
 const Inquiry = () => {
+
+    const [name,setName]=useState<string>("");
+    const [location,setLocation]=useState<string>("");
+    const [phone,setPhone]=useState<number>();
+    const [email,setEmail]=useState<string>("");
+    const [message,setMessage]=useState<string>("");
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+    
+      
+
   return (
     <div className="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
 
   <div className="text-3xl mb-6 text-center ">
-    Ready to build your website?
+    Please fill the form below 
   </div>
 
   <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
 
     <div className="col-span-2 lg:col-span-1">
-      <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name"/>
+      <input type="text" 
+      onChange={(e)=>{setName(e.target.value)}}
+      required 
+      className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Full Name"/>
     </div>
 
     <div className="col-span-2 lg:col-span-1">
-      <input type="text" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Email Address"/>
+      <input type="text" 
+      onChange={(e)=>{setLocation(e.target.value)}}
+      required 
+      className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Last known location"/>
+    </div>
+    
+    <div className="col-span-2 lg:col-span-1">
+      <input type="number" 
+      required 
+      className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="phone"/>
+    </div>
+
+    <div className="col-span-2 lg:col-span-1">
+      <input type="text" 
+      required 
+      className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Email Address"/>
     </div>
 
     <div className="col-span-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Inquiry from './components/inquiry'
 
 export default function Home() {
   return (
    <div>
-    
+    <Inquiry/>
    </div> 
   )
 }
